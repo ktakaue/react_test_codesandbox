3bd56370fa39373a3ca274d90a606bcfa9c00ffa
ktakaue <75979960+ktakaue@users.noreply.github.com>
2021-10-17 11:02:51 +0900

    useStateとuseEffectを用いた簡単なアプリケーション完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColortfulMesage";
 const App = () => {
-  const onClickCountUp = () => setNum(num + 1);
   const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowflag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    console.log("useeffect!");
+  }, [num]);
 
   return (
     <>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です!</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ!</button>
+      <br />
+      <button onClick={onClickSwitchShowflag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>( ＾∀＾)</p>}
     </>
   );
 };
